f940fff3e659b1bae77e0987d0e131b5ab88008b
ritaly <malgorzata.lyczywek@gmail.com>
2021-03-10 18:20:55 +0100

    add method with return

--- 04_methods/example.py
+++ 04_methods/example.py
@@ -6,11 +6,19 @@ def ask_about_mood():
 def show(user_response):
     print('^^^', user_response, '^^^')
 
+
+def multiply_sentence(counter):
+    sentence = "I love Python!"
+    return sentence * counter
+
 # main part of the code
 print('*********')
 ask_about_mood()
 
 response = input('--> answer here: ')
 show(response)
+
+exclamation = multiply_sentence(5)
+print(exclamation)
 print('-----END----')
 
