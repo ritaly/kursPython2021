ebafd12643ed572a3a3e5d4d01fe96727ac96c66
ritaly <malgorzata.lyczywek@gmail.com>
2021-03-17 21:14:07 +0100

    add 9

--- 04_methods/08_card.py
+++ 04_methods/08_card.py
@@ -15,20 +15,42 @@
 #
 # Gdy program będzie poprawnie działał, pozmieniaj wartości słownika (ale nie klucze!), aby zobaczyć, czy progam również zmienia swoje zachowanie.
 
-def show_is_vintage(age):
+def check_is_old(age):
     if age >= 25:
-        print(f'Gratulacje! Twój samochód {car["brand"]} może być zarejestrowany jako zabytek.')
+        print('Wiek uprawniający do rejestracji zabytku')
     else:
-        print(f'Twój samochód {car["brand"]} jest jeszcze zbyt młody na rejestracje jako zabytek.')
+        print('Samochód za młody na zabytek')
+
+def check_is_orignal(value):
+    if value:
+        print('Samochód ma 75% oryginalnych czesci')
+    else:
+        print('Nie spełnia wymogu oryginalnych czesci')
+
+def show_is_vintage(age, original):
+    check_is_old(age)
+    check_is_orignal(original)
+
+def register_car(age, original):
+    show_is_vintage(age, original)
+    print('---------------------')
+    if age >= 25 and original:
+        print(f'Gratulacje! Zarejestrowano!')
+    elif age < 25:
+        print(f'Nie można zarejestrować. Twój samochód jest jeszcze zbyt młody na rejestracje jako zabytek.')
+    elif not original:
+        print(f'Nie można zarejestrować. Twój samochód nie ma 75% oryginalnych części')
+
 
 car = {
     'brand': 'renault',
     'model': 'megan',
-    'year': 1993
+    'year': 1993,
+    'orignal': True
 }
 
 print(car)
 
 car_age = 2021 - car['year']
-show_is_vintage(car_age)
+register_car(car_age, car['orignal'])
 
