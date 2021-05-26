dc4689ff8a618c1971d18ca1ded51ffb08e97c08
ritaly <malgorzata.lyczywek@gmail.com>
2021-05-26 19:54:56 +0200

    add post

--- 13_fastAPI/main.py
+++ 13_fastAPI/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 app.counter = 0
+
+
+class Product(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    code: Optional[str] = None
+    tax: Optional[float] = None
 
 
 @app.get("/")
@@ -19,3 +29,7 @@ def count():
     app.counter += 1
     return {"counter": app.counter}
 
+
+@app.post("/products/")
+def create_product(prod: Product):
+    return prod
